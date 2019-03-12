96807862bbab3bb1754d499047fb93d1f522858b
Anandarafi <42001648+Anandarafi@users.noreply.github.com>
2019-03-12 09:39:39 +0700

    Baru

--- form-create.php
+++ form-create.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 <form action="create.php" method="POST">
-<h1><b>Tambah Data Matakuliah</b></h1>
+<h1><b>Tambah Data Dosen</b></h1>
 Nama Dosen  : <input type="text" name="nama_dosen"><br>
-MataKuliah : <input type="text" name="nama_matkul"><br>
-<input type="submit" name="btnSimpan" value="Simpan">
+No. Telepon : <input type="text" name="telp"><br>
+<td><input type="submit" name="btnSimpan" value="Simpan"></td>
 <html>
